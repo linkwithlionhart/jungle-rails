341dc309dc40e28d9305dae9a874588c6fdd6910
linkwithlionhart <linkwithlionhart@gmail.com>
2023-12-24 10:53:02 -0800

    add tests for new authentication method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,20 @@ class SessionsController < ApplicationController
   def new
   end
 
+  # def create
+  #   user = User.find_by(email: params[:session][:email])
+  #   if user && user.authenticate(params[:session][:password])
+  #     session[:user_id] = user.id
+  #     redirect_to products_path, notice: 'Logged in!'
+  #   else
+  #     Rails.logger.error("Login failed for email: #{params[:session][:email]}")
+  #     redirect_to login_path, alert: 'Email or password is incorrect.'
+  #   end
+  # end
+
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
+    user = User.authenticate_with_credentials(params[:session][:email], params[:session][:password])
+    if user
       session[:user_id] = user.id
       redirect_to products_path, notice: 'Logged in!'
     else
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   
   describe 'Validations' do
-    # Example 1: Ensure a user is valid with valid attributes
+    # Example 1: Ensure a user is valid with valid attributes.
     it 'is valid with valid attributes' do
       user = User.new(
         email: 'test@example.com',
@@ -15,7 +15,7 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
     end
 
-    # Example 2: Validate presence of password and password_confirmation
+    # Example 2: Validate presence of password and password_confirmation.
     it 'is not valid without matching password and password_confirmation' do
       user = User.new(
         email: 'test@example.com',
@@ -28,14 +28,14 @@ RSpec.describe User, type: :model do
       expect(user.errors.full_messages).to include("Password confirmation doesn't match Password.")
     end
 
-    # Example 3: Validate presence of email, first name, and last name
+    # Example 3: Validate presence of email, first name, and last name.
     it 'is not valid without required fields' do
       user = User.new
       user.valid?
       expect(user.errors.full_messages).to include("Email can't be blank.", "Password can't be blank.", "First name can't be blank.", "Last name can't be blank.")
     end
 
-    # Example 4: Validate uniqueness of email (case-insensitive)
+    # Example 4: Validate uniqueness of email (case-insensitive).
     it 'is not valid with a non-unique email' do
       User.create(
         email: 'test@example.com',
@@ -56,7 +56,7 @@ RSpec.describe User, type: :model do
       expect(user.errors.full_messages).to include("Email has already been taken.")
     end
 
-    # Example 5: Validate minimum length of the password
+    # Example 5: Validate minimum length of the password.
     it "is not valid with a password less than the minimum length" do 
       user = User.new(
         email: 'test@example.com',
@@ -68,6 +68,78 @@ RSpec.describe User, type: :model do
       user.valid?
       expect(user.errors.full_messages).to include("Password is too short. Minimum is 12 characters.")
     end
+  end # 1st describe block
 
-  end # describe block
+  describe '.authenticate_with_credentials' do
+    # Example 1: Authenticate user with correct credentials.
+    it 'returns the user instance for valid credentials' do
+      user = User.create(
+        email: 'test@example.com',
+        password: 'password',
+        password_confirmation: 'password',
+        first_name: 'John',
+        last_name: 'Doe'
+      )
+
+      authenticated_user = User.authenticate_with_credentials('test@example.com', 'password')
+      expect(authenticated_user).to eq(user)
+    end
+
+    # Example 2: Authenticate user with incorrect email.
+    it 'returns nil for incorrect email' do
+      user = User.create(
+        email: 'test@example.com',
+        password: 'password',
+        password_confirmation: 'password',
+        first_name: 'John',
+        last_name: 'Doe'
+      )
+
+      authenticated_user = User.authenticate_with_credentials('wrong@example.com', 'password')
+      expect(authenticated_user).to be_nil
+    end
+
+    # Example 3: Authenticate user with incorrect password.
+    it 'returns nil for incorrect password' do
+      user = User.create(
+        email: 'test@example.com',
+        password: 'password',
+        password_confirmation: 'password',
+        first_name: 'John',
+        last_name: 'Doe'
+      )
+
+      authenticated_user = User.authenticate_with_credentials('test@example.com', 'wrongpassword')
+      expect(authenticated_user).to be_nil
+    end
+
+    # Example 4: Authenticate user with leading/trailing spaces in email.
+    it 'ignores leading/trailing spaces in email' do
+      user = User.create(
+        email: 'test@example.com',
+        password: 'password',
+        password_confirmation: 'password',
+        first_name: 'John',
+        last_name: 'Doe'
+      )
+
+      authenticated_user = User.authenticate_with_credentials('  test@example.com  ', 'password')
+      expect(authenticated_user).to eq(user)
+    end
+
+    # Example 5: Authenticate user with case-insensitive email.
+    it 'ignores case in email' do
+      user = User.create(
+        email: 'test@example.com',
+        password: 'password',
+        password_confirmation: 'password',
+        first_name: 'John',
+        last_name: 'Doe'
+      )
+
+      authenticated_user = User.authenticate_with_credentials('TEST@example.com', 'password')
+      expect(authenticated_user).to eq(user)
+    end
+  end # 2nd describe block
+
 end # outer-most
